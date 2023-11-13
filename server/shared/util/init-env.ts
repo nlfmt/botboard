55d8f906583e754634b7e7781cb39d856643cf1c
nlfmt <nlfmt@gmx.de>
2023-11-13 02:38:20 +0100

    better error printing logic in init-env

--- server/shared/util/init-env.ts
+++ server/shared/util/init-env.ts
@@ -1,29 +1,30 @@
-import { ZodError } from "zod"
 import dotenv from "dotenv"
 import { envModel } from "@/env"
-
-export function onInvalidEnv(err: ZodError) {
-  console.error("\n❌ Invalid environment variables:")
-  for (const entry of Object.entries(err.flatten().fieldErrors)) {
-    console.error(` - ${entry[0]}: ${entry[1]}`)
-  }
-  console.error("\n")
-  process.exit(1)
-}
 
 export function initEnv(path: string) {
   const res = dotenv.config({
     path,
   })
+
   if (res.parsed) {
     process.env = { ...process.env, ...res.parsed }
-  } else {
+  }
+
+  if (res.error) {
     console.error("\n❌ Invalid environment variables:")
     console.error(` - ${res.error}`)
     console.error("\n")
     process.exit(1)
   }
-  console.log(path, process.env.GITHUB_CLIENT_ID)
+
   const result = envModel.safeParse(process.env)
-  if (!result.success) onInvalidEnv(result.error)
+
+  if (!result.success) {
+    console.error("\n❌ Invalid environment variables:")
+    for (const entry of Object.entries(result.error.flatten().fieldErrors)) {
+      console.error(` - ${entry[0]}: ${entry[1]}`)
+    }
+    console.error("\n")
+    process.exit(1)
+  }
 }
